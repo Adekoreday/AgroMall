474b1b2aba2b9ae78dc06f519bce85d4051126cd
Adekoreday <khord4eng@gmail.com>
2020-05-23 08:58:46 +0100

    builds

---
+++ build/precache-manifest.9e9ff5c22323aafd7c53b0b22f8dfdb7.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69e62feea960bac8264872bf63eb1d0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fb06a1a33880c174389",
+    "url": "/static/css/main.b651958d.chunk.css"
+  },
+  {
+    "revision": "b2be94fe675c80b3fa11",
+    "url": "/static/js/2.e006f211.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e006f211.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fb06a1a33880c174389",
+    "url": "/static/js/main.a2fea5f1.chunk.js"
+  },
+  {
+    "revision": "4ef7f9a9baedfeacf805",
+    "url": "/static/js/runtime-main.dae049e6.js"
+  },
+  {
+    "revision": "daf07ce3207f238df12b54952bd4f8a7",
+    "url": "/static/media/agricModel.daf07ce3.svg"
+  },
+  {
+    "revision": "5b9129eaa141a03131aa0f5e3e36cac3",
+    "url": "/static/media/association.5b9129ea.svg"
+  },
+  {
+    "revision": "19ab3ebcc1d74d7dad2728af516f68ab",
+    "url": "/static/media/background.19ab3ebc.svg"
+  },
+  {
+    "revision": "e3a1a61273b16b33a559be8977fa45df",
+    "url": "/static/media/cola.e3a1a612.svg"
+  },
+  {
+    "revision": "bd11fe7c587822f681669fb9be5c3412",
+    "url": "/static/media/commitment.bd11fe7c.svg"
+  },
+  {
+    "revision": "f34d2fbd75def0c315142281899aa397",
+    "url": "/static/media/community.f34d2fbd.svg"
+  },
+  {
+    "revision": "0faab4e8c0d01035b792616c73112f72",
+    "url": "/static/media/coverImg.0faab4e8.svg"
+  },
+  {
+    "revision": "20e7c14292385cdca11874638a6eb3af",
+    "url": "/static/media/coverImgfive.20e7c142.svg"
+  },
+  {
+    "revision": "af5eb3a8f10d81dca0e160bcfedbc94f",
+    "url": "/static/media/coverImgfour.af5eb3a8.svg"
+  },
+  {
+    "revision": "223e02a6576a156c3689335322115c1a",
+    "url": "/static/media/coverImgseven.223e02a6.svg"
+  },
+  {
+    "revision": "47cf025e0a73e4716cfa142844569ed4",
+    "url": "/static/media/coverImgsix.47cf025e.svg"
+  },
+  {
+    "revision": "65ad0840fdf11dd1b3a3615c57cf480e",
+    "url": "/static/media/coverImgthree.65ad0840.svg"
+  },
+  {
+    "revision": "223e02a6576a156c3689335322115c1a",
+    "url": "/static/media/coverImgtwo.223e02a6.svg"
+  },
+  {
+    "revision": "4348d10bac89ddeee476f9c52e3fb7d3",
+    "url": "/static/media/createAccount.4348d10b.svg"
+  },
+  {
+    "revision": "b4e2183c6c49077379531fc0a02836ae",
+    "url": "/static/media/facebooklogo.b4e2183c.svg"
+  },
+  {
+    "revision": "31860236b30766c7ffa39b9fb33ed9ff",
+    "url": "/static/media/farmImg.31860236.svg"
+  },
+  {
+    "revision": "653bf3cfb51d46c867b7a9ab7a1cb98b",
+    "url": "/static/media/heroImg.653bf3cf.svg"
+  },
+  {
+    "revision": "01b3bc79ad33c3ebc45756a5d308da58",
+    "url": "/static/media/instagramlogo.01b3bc79.svg"
+  },
+  {
+    "revision": "76dce0c11b5e8a6f83c763b6147c312b",
+    "url": "/static/media/logo.76dce0c1.svg"
+  },
+  {
+    "revision": "36794a07dc0af8dec89bc1eead04c30e",
+    "url": "/static/media/logoGreen.36794a07.svg"
+  },
+  {
+    "revision": "2486b7dccd28a6dd03355ff72f24cedc",
+    "url": "/static/media/mission.2486b7dc.svg"
+  },
+  {
+    "revision": "e4b96f38c3443fde32c8377ccde2b449",
+    "url": "/static/media/newdeal.e4b96f38.svg"
+  },
+  {
+    "revision": "0fdb72de1df360434a8e5dc77d377d34",
+    "url": "/static/media/partners.0fdb72de.svg"
+  },
+  {
+    "revision": "5cc818f04c0c0c191c2c119980b0b371",
+    "url": "/static/media/picture.5cc818f0.svg"
+  },
+  {
+    "revision": "3d25cb937eda2d76980c92305ebdd0b7",
+    "url": "/static/media/policyIcon.3d25cb93.svg"
+  },
+  {
+    "revision": "3893ece0cd98fc54d2b8ff6a4d6aa979",
+    "url": "/static/media/research.3893ece0.svg"
+  },
+  {
+    "revision": "0283f1816644cd888d7ee2c9343280b6",
+    "url": "/static/media/settings.0283f181.svg"
+  },
+  {
+    "revision": "f50f8fd51bb4bd7bb0e06ccc30b26994",
+    "url": "/static/media/tractor.f50f8fd5.svg"
+  },
+  {
+    "revision": "29b990216bb42382b6f061c63c27e2d7",
+    "url": "/static/media/twitterlogo.29b99021.svg"
+  }
+]);
